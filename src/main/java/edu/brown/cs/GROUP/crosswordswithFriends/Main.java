57a1dcfc27c01ce3638790cd3475fba723c1c727
Steffani Gomez <steffani_gomez@brown.edu>
2016-03-19 15:08:52 -0400

    setting up project to work with eclipse, maven, and the command line

---
+++ src/main/java/edu/brown/cs/GROUP/crosswordswithFriends/Main.java
@@ -0,0 +1,12 @@
+package edu.brown.cs.GROUP.crosswordswithFriends;
+
+public final class Main {
+
+  /**
+   * Initializes the program being run.
+   * @param args the arguments from the command line
+   */
+  public static void main(String[] args) {
+  	System.out.println("Hi");
+  }
+}
