9c00ee0fb7f2c784d5a05fedccdf7c174ad4f31a
Avery Crits-Christoph <avery_crits-christoph@brown.edu>
2016-04-18 20:08:14 -0400

    can't figure out git ignore

--- target/classes/static/js/main.js
+++ target/classes/static/js/main.js
@@ -1,3 +1,11 @@
+function checkWord(word, x, y, o){
+	console.log(word+" "+x+" "+y+" "+o);
+	$.get("/check", {word: word, x: x, y: y, orientation: o, id:"abcdef"}, function(response) {
+        var res = JSON.parse(response);
+        console.log(res);
+    });
+}
+
 function orient(){
 	var classes = $(".active").attr("class").split(" ");
 	$(".wordActive").removeClass("wordActive");
@@ -9,39 +17,121 @@ function orient(){
 }
 
 function nextRow(row){
-	var nex = 
-	return "r"+nextRow(row)
+	return (row+1)%numRow;
 }
+
+function prevRow(row){
+	row = row-1;
+	if (row < 0){
+		row = numRow -1;
+	}
+	return row;
+}
+
 
 function nextCol(col){
 	return (col+1)%numCol;
 }
 
-function next(){
+function prevCol(col){
+	col = col-1;
+	if (col < 0){
+		col = numCol -1;
+	}
+	return col;
+}
+
+function next(dir){
 	var classes = $(".active").attr("class").split(" ");
 	$(".active").removeClass("active");
+	
 	var row = row = parseFloat(classes[2][1]);
 	var col = parseFloat(classes[1][1]);
+	
+	var found = false;
+	var done = true;
+	var word = "";
+	
+	var startX = null;
+	var startY = null;
+	
 	if (orientation == "down"){
-		var col = $(".c"+col);
-		var next = "r"+nextRow(row);
-		for (var i=0; i<numCol; i++){
-			var block = col[i];
-			if ($(block).hasClass(next)){
-				$(block).addClass("active");
-				$(block).focus();
+		var c = $(".c"+col);
+		var next;
+		if (dir == -1){
+			next = prevRow(row);
+		} else {
+			next = nextRow(row);
+		}
+		for (var i=0; i<numRow; i++){
+			var block = c[i];
+			if (!found){
+				if ($(block).hasClass("r"+next)){
+					if ($(block).hasClass("filled")){
+						if (dir == -1){
+							next = prevRow(next);
+						} else {
+							next = nextRow(next);
+						}
+						i=-1;
+					} else {
+						$(block).addClass("active");
+						$(block).focus();
+						found = true;
+					}
+				}
+			}
+			if (!$(block).hasClass("filled") && $(block).val() == ""){
+				done = false;
+			}
+			else {
+				if (!$(block).hasClass("filled") && startY == null){
+					startX = col;
+					startY = i;
+				}
+				word+= $(block).val();
 			}
 		}
+		if (done){
+			checkWord(word, startX, startY, "DOWN");
+		}
 	} else if (orientation == "across"){
-		//$(".c"+nextCol(col)+" .r"+row).addClass("active");
-		var row = $(".r"+row);
-		var next = "c"+nextCol(col);
-		for (var i=0; i<numRow; i++){
-			var block = row[i];
-			if ($(block).hasClass(next)){
-				$(block).addClass("active");
-				$(block).focus();
+		var r = $(".r"+row);
+		if (dir == -1){
+			next = prevCol(col);
+		} else {
+			next = nextCol(col);
+		}
+		for (var i=0; i<numCol; i++){
+			var block = r[i];
+			if (!found){
+				if ($(block).hasClass("c"+next)){
+					if ($(block).hasClass("filled")){
+						if (dir == -1){
+							next = prevCol(next);
+						} else {
+							next = nextCol(next);
+						}
+						i=-1;
+					} else {
+						$(block).addClass("active");
+						$(block).focus();
+						found = true;
+					}
+				} 
 			}
+			if ($(block).val() == ""){
+				done = false;
+			} else {
+				if (!$(block).hasClass("filled") && startX == null){
+					startX = i;
+					startY = row;
+				}
+				word+= $(block).val();
+			}
+		}
+		if (done){
+			checkWord(word, startX, startY, "ACROSS");
 		}
 	}
 }
@@ -66,21 +156,39 @@ window.onload = function(response) {
 	$("textarea").keydown(function(event) {
 	     switch(event.keyCode){
 	        case 37:
-	        	orientation = "across";
-	        	orient();
+	        	if (orientation=="across"){
+	        		next(-1);
+	        	}else {
+		        	orientation = "across";
+		        	orient();
+	        	}
 	        	break;
 	        case 38:
-	        	orientation = "down";
-	        	orient();
+	        	if (orientation=="down"){
+	        		next(-1);
+	        	}else {
+		        	orientation = "down";
+		        	orient();
+	        	}
 	        	break;
 	        case 39:
-	        	orientation = "across";
-	        	orient();
+	        	if (orientation=="across"){
+	        		next(1);
+	        	}else {
+		        	orientation = "across";
+		        	orient();
+	        	}
 	        	break;
 	        case 40:
-	        	orientation = "down";
-	        	orient();
+	        	if (orientation=="down"){
+	        		next(1);
+	        	}else {
+		        	orientation = "down";
+		        	orient();
+	        	}
 	        	break;
+	        case 8:
+	        	$(this).val("");
 	        default:
 	        	if (event.keyCode>64 && event.keyCode<90){
 	        		$(this).val(String.fromCharCode(event.keyCode));
